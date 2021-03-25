02265a9c7c15f337a91e1cff6d74d6c68d91e4aa
jayamkay <nikolayzaporozez@gmail.com>
2021-03-25 17:46:42 +0200

    Props

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,13 @@ import Post from './Post/Post';
 
 
 const MyPosts = () => {
-    return <div className='posts'>
-      
+  return <div className='posts'>
     <AddPost />
     <div>
-      <Post />
-      <Post />
-      <Post />
+      <Post message='This is the First post' />
+      <Post message="It's the Second post" />
     </div>
-    </div>
+  </div>
 }
 
 
--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,14 @@
 import postCSS from './Post.module.css';
 
 
-const Post = () => {
+const Post = (props) => {
+
+// console.log(props.message); //Это строка из js функции, а не из jsx, потому фигурные скобки не нужны
+debugger;
+
 return <div className={postCSS.post}>
     <img src='https://railsware.com/blog/wp-content/uploads/2019/07/Why-we-use-ReactJS-for-our-projects-Icon.png'></img>
-    <p>Post 1</p>
+    <p>{props.message}</p>
 </div>
 }
 
