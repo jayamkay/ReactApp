9133c1dbd80fd65144d07b7701156ffdf76f2a78
jayamkay <nikolayzaporozez@gmail.com>
2021-04-16 10:51:20 +0300

    ref, onClick, VirtualDOM

--- src/Components/Profile/MyPosts/Add post/AddPost.jsx
+++ src/Components/Profile/MyPosts/Add post/AddPost.jsx
@@ -1,13 +1,41 @@
+import React from 'react';
 import addpostCSS from './AddPost.module.css';
 
 
-const AddPost = () => {
+
+// const AddPost = () => {
+//     return <div className={addpostCSS.addpost}>
+//     <h5>My posts</h5>
+//     <textarea className="form-control" rows="4" placeholder="your news..." id='textArea'></textarea>
+//     <button type="button" class="btn btn-success">Send</button>
+//     </div>
+// }
+
+function AddPost(props)
+{
+    function showAlert()
+    {
+        //let text = document.getElementById('textArea').value;
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
+    let showAlert2 = () => {
+        showAlert()
+        alert('Test2');
+    }
+
+
+    let newPostElement = React.createRef();
+
     return <div className={addpostCSS.addpost}>
     <h5>My posts</h5>
-    <textarea className="form-control" rows="4" placeholder="your news..."></textarea>
-    <button type="button" class="btn btn-success">Send</button>
+    <textarea className="form-control" rows="4" placeholder="your news..." id='textArea' ref={newPostElement }></textarea>
+    <button type="button" class="btn btn-success" onClick={showAlert2}>Send</button>
+    {/* <button type="button" class="btn btn-success" onClick={showAlert} onClick={showAlert2}>Send</button> */}
+    {/* !!!ВНИМАНИЕ!!!При отдаче функции атрибуту onClick, () не ставить, иначе функция будет вызвана сразу */}
+    {/* Атрибут onClick= нельзя добавлять 2 раза. Все кроме последнего будут игнорироваться */}
     </div>
 }
-
 
 export default AddPost;
